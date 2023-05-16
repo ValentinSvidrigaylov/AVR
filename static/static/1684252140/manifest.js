305b36bf326bedcb8fe1e6fc45e76dd47bfb4e39
Svidrigaylov <sergeyshe07@gmail.com>
2023-05-16 18:52:37 +0300

    Updates

--- static/static/1684252140/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fabout","\u002Fusage","\u002Fhistory","\u002Fsources","\u002Farchitecture"]})
--- static/static/1684252140/sources/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstatic\u002Fstatic\u002F1684252140",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsources",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fstatic\u002F",cdnURL:a}}}}(null));
