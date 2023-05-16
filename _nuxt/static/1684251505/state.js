d97e24bd38a4960f148f9c8c0dafb1600512967d
Svidrigaylov <sergeyshe07@gmail.com>
2023-05-16 18:49:08 +0300

    Updates

--- _nuxt/static/1684251505/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"nuxt_test\u002F_nuxt\u002Fstatic\u002F1684251505",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"nuxt_test\u002F",assetsPath:"nuxt_test\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ static/static/1684252140/history/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstatic\u002Fstatic\u002F1684252140",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhistory",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fstatic\u002F",cdnURL:a}}}}(null));
